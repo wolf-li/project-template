ccae7f1d58542640a0cdadb0c269f56b5bf26211
wolf-li <59823739+wolf-li@users.noreply.github.com>
2024-11-10 21:08:52 +0800

    Create main.c

---
+++ C/cli-template/main.c
@@ -0,0 +1,17 @@
+/*
+ date: 
+ auth: wolf-li
+ email: hermannli2019@163.com
+ version: v0.0.1
+ description:
+
+ platform: 
+ build-tool: gcc make
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
